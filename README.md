f60873d47cf7020798d79fe01ece11f81a885503
Joey <sharpshooter30@gmail.com>
2018-02-15 15:38:42 -0700

    starting to work on the idea seems to be that adding different bases happens automatically

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # concept-code
 Using this as a save place for code ideas to test on multiple platforms
+for fun ideas or one off coding experiments and learning to use dynamics
+and teach myself ways of using tools
--- gurka_experiment.cc
+++ gurka_experiment.cc
@@ -3,12 +3,38 @@
 
 using namespace std;
 
-int main (int argc, char *argv[]) {
+
+void test_adding(int octal, int dec, int hex)
+{
+        
+    printf("the decimal = %d the octal = %d the hex = %d\n", dec, octal, hex);
+
+    printf("lets try adding base 10 and 8 together %d", dec + octal);
+    
+    printf("lets try adding base 8 and 16 together %d", octal + hex);
+
+    printf("lets try adding base 10 and 16 together %d", dec + hex);
+
+}
+
+void test_subtraction(int octal, int dec, int hex)
+{
+    
+
+}
+
+int main (int argc, char *argv[])
+{
     int octal = 0;  // octal number to play with
     int hex = 0x0;  // hex number because why not
     int dec = 0;    // decimal number can't tell the differince
 
+    octal = 012;
+    dec = 10;
+    hex = 0x10;
 
+
+    test_adding(octal, dec, hex);
 
 
 }
