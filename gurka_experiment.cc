393fd6ccaeb28f92d47a3216007940803af44e78
Joey <sharpshooter30@gmail.com>
2018-02-15 15:03:12 -0700

    had an intresting idea writing code to test it and see what happens

---
+++ gurka_experiment.cc
@@ -0,0 +1,14 @@
+// Cool idea to test the effeccts of using the octal with descimal
+#include  <stdio.h>
+
+using namespace std;
+
+int main (int argc, char *argv[]) {
+    int octal = 0;  // octal number to play with
+    int hex = 0x0;  // hex number because why not
+    int dec = 0;    // decimal number can't tell the differince
+
+
+
+
+}
