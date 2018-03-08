a3e49df65d5e2020702f149cbfb0d2b7c2dd8cc6
Joey <sharpshooter30@gmail.com>
2018-03-08 14:10:18 -0700

    pushing lastest changes

--- gurka_experiment.cc
+++ gurka_experiment.cc
@@ -1,7 +1,8 @@
 // Cool idea to test the effeccts of using the octal with descimal
 #include  <stdio.h>
+//#include <iostream>
 
-using namespace std;
+//using namespace std;
 
 
 void test_adding(int octal, int dec, int hex)
@@ -13,27 +14,67 @@ void test_adding(int octal, int dec, int hex)
 
     printf("lets try adding base 10 and 16 together %d\n", dec + hex);
 
+    printf("another test using numbers base 8 and 10 should as always %d\n", 012 + 10);
+
 }
 
 void test_subtraction(int octal, int dec, int hex)
 {
+    printf("lets try subtracting base 10 and 8 together %d\n", dec - octal);
     
+    printf("lets try subtracting base 8 and 16 together %d\n", octal - hex);
+
+    printf("lets try subtracting base 10 and 16 together %d\n", dec - hex);
+
+    printf("another test using numbers base 8 and 10 should as always %d\n", 012 - 10);
+
 
 }
 
 int main (int argc, char *argv[])
 {
-    int octal = 0;  // octal number to play with
-    int hex = 0x0;  // hex number because why not
-    int dec = 0;    // decimal number can't tell the differince
+    char answer;
+    const char *value[3] = {"decimal", "hex (repsented by 0x#)", "octal (represented by 0#)"};
 
-    octal = 012;
-    dec = 10;
-    hex = 0xA;
+    int math[3] = {0, 0x0, 00};
+    int test1 = 0;
+    int test2 = 2;
 
-    printf("the decimal = %d the octal = %d the hex = %d\n", dec, octal, hex);
 
-    test_adding(octal, dec, hex);
+    printf("Would you like to define you own values?\n");
+    scanf(" %c", &answer);
+    
+
+    if ((answer == 'Y') || (answer == 'y'))
+    {
+        for(int i = 0; i < 3; i++)
+        {
+            printf("Enter %s value\n", value[i]);
+
+            scanf("%d", &math[i]);
+
+        }   
+
+    }
+
+    else
+    {
+        math[2] = 012;
+        math[0] = 10;
+        math[1] = 0xA;
+
+    }
+
+
+    printf("the decimal = %d the octal = %d the hex = %d\n", math[0], math[2], math[1]);
+    
+    printf("addition\n");
+
+    test_adding(math[2], math[0], math[1]);
+    
+    printf("subtraction\n");
+
+    test_subtraction(math[2], math[0], math[1]);
 
 
 }
