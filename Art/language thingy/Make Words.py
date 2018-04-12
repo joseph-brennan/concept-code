64f54d0e2c5c02ec68f3efefb5c27e771f7b7560
Joey <sharpshooter30@gmail.com>
2018-04-12 13:22:43 -0600

    small edits

--- Art/language thingy/Make Words.py
+++ Art/language thingy/Make Words.py
@@ -1,26 +1,25 @@
 import random
+import tkinter
 
-"""
-now mine 
-"""
+
 class WordGenerator:
     def __init__(self):
         self.vowel_choices = []
         self.consonant_choices = []
-        self.syllable_choices = []
+        self.syllable_choices = {}
 
     def syllable(self):
-        self.syllable_choices = ["CV", "VC", "CVC"]
+        self.syllable_choices = {1: "CV", 2: "VC", 3: "CVC"}
 
         # return random.choice(choices)
 
     def vowel(self):
-        self.vowel_choices = "a"
+        self.vowel_choices = ["a", "e", "i", "o", "u"]
 
         # return random.choice(choices)
 
     def consonant(self):
-        self.consonant_choices = "b"
+        self.consonant_choices = ["b", "c", "d", "f", "h", "j", "k", "l", "m", "n", "p", "q", "r", "t", "v", "x", "z"]
 
         # return random.choice(choices)
 
@@ -29,43 +28,62 @@ class WordGenerator:
 
         word, temp = "", ""
 
+        for i in range(0, length):
+
+            temp += random.choice(list(self.syllable_choices.values()))
+
+        # this says for each letter in the "word" template. where letter is the C or V
+        for letter in temp:
+            if letter in 'C':
+                word += random.choice(self.consonant_choices)
+
+            elif letter in 'V':
+                word += random.choice(self.vowel_choices)
+
+            else:
+                print("how? fell out of word building")
+                exit(1)
+
+        return word
+
+    def run(self):
         # hard coded for now to use the options above
         self.syllable()
         self.consonant()
         self.vowel()
 
-        for i in range(0, length):
-
-            temp += random.choice(self.syllable_choices)
-
-            print("temp {}".format(temp))
-
-        # this says for each letter in the "word" template. where letter is the C or V
-        for letter in temp:
-            if letter == 'C':
-                word += random.choice(self.consonant_choices)
-
-                print("C {}".format(word))
-
-            else:
-                word += random.choice(self.vowel_choices)
-
-                print("V {}".format(word))
-
-        return word
-
-    def run(self):
         while True:
-            numsyll = int(input("Enter number of syllables: "))
+            try:
+                numsyll = int(input("Enter number of syllables: "))
 
-            if numsyll == -1:
+            except ValueError:
+                numsyll = input("Please enter an actual number: ")
+
+                if numsyll.isdigit():
+                    numsyll = int(numsyll)
+
+                else:
+                    print("Still not a number aborting.")
+                    exit(-1)
+
+            if numsyll is -1:
                 break
 
+            elif numsyll is 0:
+                numsyll = random.randint(1, 4)
+
             else:
-                word = self.word(numsyll)
-                print("{}\n".format(word))
+                pass
+
+            word = self.word(numsyll)
+
+            print("{}\n".format(word))
 
 
 if __name__ == '__main__':
-    word = WordGenerator()
-    word.run()
+    tk = tkinter
+    tk.Tk()
+    tk.mainloop()
+
+    words = WordGenerator()
+    words.run()
